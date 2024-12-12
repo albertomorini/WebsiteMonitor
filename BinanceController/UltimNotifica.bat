6652c19049ebf770a937b90ca57e7476ac65ab23
99morini@gmail.com <99morini@gmail.com>
2024-12-12 20:08:17 +0100

    cleaning

--- BinanceController/UltimNotifica.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\alberto.morini\AppData\Local\Programs\Python\Python313\python.exe  .\binance.py UltimaNotifica %*
-pause
--- BinanceController/bat.bat
+++
@@ -1,2 +0,0 @@
-C:\Users\alberto.morini\AppData\Local\Programs\Python\Python313\python.exe  C:\Users\alberto.morini\Downloads\test.py %*
-pause
