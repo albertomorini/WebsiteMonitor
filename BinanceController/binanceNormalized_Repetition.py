a87ac3373431796ed737a5601a49a16cc60267ea
albertomorini <99morini@gmail.com>
2025-10-18 11:30:30 +0200

    keep in wallet when sold by top

--- BinanceController/binanceNormalized_Repetition.py
+++ BinanceController/binanceNormalized_Repetition.py
@@ -197,7 +197,7 @@ def compareRegisters(actual):
                         ### CONVERT - OUTCOME::SELL
                         if(getSymbolWOBase(symbol) in WALLET): ## if still on wallet, to avoid the double sell that would go to error dued to double couple USDT and USD
                             binanceConverter.acceptPropose(getSymbolWOBase(symbol),CONVERT_SYMBOL,binanceConverter.getAmount(getSymbolWOBase(symbol)))
-                            WALLET.remove(getSymbolWOBase(symbol))
+                            ###WALLET.remove(getSymbolWOBase(symbol)) ##---> 18Oct2025 keep in wallet when sold by top
                     elif(percentageIncrement>INCREMENT_PERCENTAGE): # Up the increment counter - currency is growning ## ~ se PREZZO ATTUALE > del 0,5% di PREZZO ALTO :  # case 1
                         incrementCounter += 1 #if up, increment the counter -- contatore notifica
                         max_price=new_price ## update max_price
